c5574dda5aab93c963c672f56208230299325f70
maharjansujan <sujanmaharjan40@gmail.com>
2023-07-15 19:29:55 +0545

    change css

--- src/components/views/auth/index.jsx
+++ src/components/views/auth/index.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
             });
     }
 
-    const onSignup = async (e) => {
+    const onSign = async (e) => {
         e.preventDefault();
 
         await createUserWithEmailAndPassword(auth, email, password)
@@ -48,50 +48,66 @@ const Auth = () => {
     }
 
     return (
+        // <div className="login-page">
         <div className="login-page">
-            <h1 className="d-flex w-100 p-4 d-flex justify-content-center pb-4">Welcome to Jhigu Grocery</h1>
-            <div className="d-flex w-100 p-4 d-flex justify-content-center pb-4">
+
+
+            <div className="d-flex w-10 p-4 d-flex justify-content-center pb-4">
                 <form>
-                    <div className="card">
-                        <div className="text-center card-title">
+                    {/* <div className="card"> */}
+                    <div className="card border border-white">
+
+                        <h3 className="d-flex w-100 p-4 d-flex justify-content-center text-center pb-4 text-white">Welcome to Jhigu Grocery</h3>
+                        <div className="text-center card-title ">
+
                             {pageType === "signup" ?
                                 <h3>Sign Up</h3>
                                 :
-                                <h3>Log In</h3>
+                                <h3 className="text-white">Log In</h3>
                             }
                         </div>
-                        <div className="card-body">
+                        {/* <div className="card-body"> */}
+                        <div className="card-body text-center">
                             {/* <!-- Email input --> */}
                             <div className="form-outline mb-4">
                                 <label className="form-label" htmlFor="form2Example1">Email address</label>
-                                <input type="email" id="form2Example1" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)}  />
+                                <input type="email" id="form2Example1" className="form-control border border-white" value={email} onChange={(e) => setEmail(e.target.value)} />
                             </div>
 
+
                             {/* <!-- Password input --> */}
-                            <div className="form-outline mb-4">
+                            <div className="form-outline mb-4 ">
                                 <label className="form-label" htmlFor="form2Example2">Password</label>
-                                <input type="password" id="form2Example2" className="form-control"  value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type="password" id="form2Example2" className="form-control border border-white" value={password} onChange={(e) => setPassword(e.target.value)} />
                             </div>
                             {pageType === "signup" ?
-                                <button type="button" className="btn btn-primary btn-block mb-4"  onClick={onSignup}>Sign up</button>
+                                <button type="button" className="btn btn-outline-primary btn-block mb-4" onClick={onSign}>Sign up</button>
                                 :
-                                <button type="button" className="btn btn-primary btn-block mb-4"  onClick={onLogin}>Sign in</button>
+                                // <button type="button" className="btn btn-primary btn-block mb-4"  onClick={onLogin}>Sign in</button>
+                                <button type="button" className="btn btn-outline-primary btn-block mb-4 text-white justify-content-center" onClick={onLogin}>Sign in</button>
                             }
+                            <p className="small mb-1 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+
+                            <div className="d-flex w-100 p-4 d-flex justify-content-center pb-4">
+                                {pageType === "signup" ?
+                                    <div className="text-center">
+                                        {/* <p>Already have an account? <button className="btn btn-primary" onClick={() => setPageType("login")}>Log in</button></p> */}
+                                        <p>Already have an account? <button className="btn btn-primary " onClick={() => setPageType("login")}>Log in</button></p>
+                                    </div>
+                                    :
+                                    <div className="text-center">
+                                        {/* <p>Don't have an account? <button className="btn btn-outline-primary text-white " onClick={() => setPageType("signup")}>Sign up</button></p> */}
+
+                                        <p>Don't have an account? <br /> <button className="btn btn-outline-primary text-white " onClick={() => setPageType("signup")}>Sign up</button></p>
+                                    </div>
+                                }
+                            </div>
                         </div>
+
                     </div>
                 </form>
             </div>
-            <div className="d-flex w-100 p-4 d-flex justify-content-center pb-4">
-                {pageType === "signup" ?
-                    <div className="text-center">
-                        <p>Already have an account? <button className="btn btn-primary" onClick={() => setPageType("login")}>Log in</button></p>
-                    </div>
-                    :
-                    <div className="text-center">
-                        <p>Don't have an account? <button className="btn btn-primary" onClick={() => setPageType("signup")}>Sign up</button></p>
-                    </div>
-                }
-            </div>   
+
         </div>
     )
 
