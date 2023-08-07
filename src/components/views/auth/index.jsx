41cd2319e28bd94d9a192af4a08e5d10b4bf758a
maharjansujan <sujanmaharjan40@gmail.com>
2023-08-07 10:39:39 +0545

    change

--- src/components/views/auth/index.jsx
+++ src/components/views/auth/index.jsx
@@ -90,7 +90,7 @@ const Auth = () => {
                             <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
 
                             <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                                <a href="https://www.facebook.com" className="text-white"><i className="bi bi-facebook fa-lg"></i></a>
+                                <a href="#!" className="text-white"><i className="bi bi-facebook fa-lg"></i></a>
                                 <a href="#!" className="text-white"><i className="bi bi-instagram fa-lg mx-6 px-2"></i></a>
                                 <a href="#!" className="text-white"><i className="bi bi-whatsapp fa-lg"></i></a>
                             </div>
